47337dbab95ea3642244a25adc2c9e01b8a880a3
Yemeda Grayson <42628679+YoitsMeda@users.noreply.github.com>
2019-01-07 16:03:56 -0600

    Add files via upload

---
+++ techjobs-mvc-master/src/main/java/org/launchcode/controllers/SearchController.java
@@ -0,0 +1,48 @@
+package org.launchcode.controllers;
+
+import org.launchcode.models.JobData;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+
+/**
+ * Created by LaunchCode
+ */
+@Controller
+@RequestMapping("search")
+public class SearchController {
+
+    @RequestMapping(value = "")
+    public String search(Model model) {
+        model.addAttribute("columns", ListController.columnChoices);
+        return "search";
+    }
+
+    // TODO #1 - Create handler to process search request and display results
+    @RequestMapping(value = "results")
+    public String results(Model model, @RequestParam String searchTerm, @RequestParam String searchType)
+    {
+    ArrayList<HashMap<String, String>> jobs;
+
+    if(searchType.equals("all"))
+    {
+    jobs = JobData.findByValue(searchTerm);
+    }
+
+    else
+    {
+    	jobs = JobData.findByColumnAndValue(searchType, searchTerm);
+    }
+    model.addAttribute("title","Search Results for \"" + searchTerm + "\":");
+    model.addAttribute("columns", ListController.columnChoices);
+    model.addAttribute("type", searchType);
+    model.addAttribute("term", searchTerm);
+    model.addAttribute("jobs", jobs);
+    return "search";
+    }
+
+}
